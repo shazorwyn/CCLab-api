19ab1a71505413a05cbe95a19ebac81e87a1225d
Aditya Kumar Gupta <adityakumar.gupta.cd.eee22@itbhu.ac.in>
2025-11-01 18:38:12 +0530

    first prototyping

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ app = FastAPI()
 
 class DeviceSignal(BaseModel):
     id: str
-    latitude: float
-    longitude: float
+    lat: float
+    lng: float
 
 
 class TargetLocation(BaseModel):
     name: str
-    latitude: float
-    longitude: float
+    lat: float
+    lng: float
 
 
 class TargetResponse(BaseModel):
@@ -24,3 +24,27 @@ class TargetResponse(BaseModel):
 @app.get("/")
 async def root():
     return {"message": "Welcome to the [] app!"}
+
+
+@app.post("/api/v1/fuel-alert", response_model=TargetResponse)
+async def fuel_alert(signal: DeviceSignal):
+    print(
+        f"Received signal from {signal.id} at ({signal.lat}, {signal.lng})")
+
+    mock_pumps = [
+        TargetLocation(
+            name="Mock Pump 1 (Nearest)",
+            lat=signal.lat + 0.005,
+            lng=signal.lng - 0.002,
+        ),
+        TargetLocation(
+            name="Mock Pump 2",
+            lat=signal.lat - 0.01,
+            lng=signal.lng + 0.007,
+        )
+    ]
+
+    return TargetResponse(
+        status="success",
+        response=mock_pumps
+    )
